f296365c431da9570195a1fc96c9603fde8e9a0f
ysansone <yamila.sansone@mercadolibre.com>
2017-09-07 20:02:10 -0300

    Elimino el titulo del action bar de la congrats

--- app/src/main/java/com/uade/tesis/evaluated/EvaluatedCongratsActivity.java
+++ app/src/main/java/com/uade/tesis/evaluated/EvaluatedCongratsActivity.java
@@ -2,10 +2,10 @@ package com.uade.tesis.evaluated;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -25,6 +25,11 @@ public class EvaluatedCongratsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_congrats);
+        final ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            supportActionBar.setTitle(null);
+        }
+
         initView();
     }
 
