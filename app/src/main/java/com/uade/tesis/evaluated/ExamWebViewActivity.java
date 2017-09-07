336047c78775a871e9055887a2d1e1adecd731e3
ysansone <yamila.sansone@mercadolibre.com>
2017-09-07 20:01:10 -0300

    Elimino el titulo del action bar

--- app/src/main/java/com/uade/tesis/evaluated/ExamWebViewActivity.java
+++ app/src/main/java/com/uade/tesis/evaluated/ExamWebViewActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -41,9 +42,13 @@ public class ExamWebViewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_evaluado);
+
+        final ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            supportActionBar.setTitle(null);
+        }
 
         webView = (WebView) findViewById(R.id.evaluado_form_web_view);
 
@@ -185,5 +190,4 @@ public class ExamWebViewActivity extends AppCompatActivity {
             }
         };
     }
-
 }
