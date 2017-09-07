102ca4a4e1462de6fbb83363a01a4901ef3db6eb
ysansone <yamila.sansone@mercadolibre.com>
2017-09-07 20:21:03 -0300

    Modifico menu

---
+++ app/src/main/java/com/uade/tesis/commons/EvaluatedMenuActivity.java
@@ -0,0 +1,54 @@
+package com.uade.tesis.commons;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import com.uade.tesis.R;
+import com.uade.tesis.evaluated.DecoderActivity;
+import com.uade.tesis.evaluated.EvaluatedCongratsActivity;
+import com.uade.tesis.evaluated.ExamWebViewActivity;
+import com.uade.tesis.evaluated.utils.EvaluatedWelcomeDialog;
+
+public class EvaluatedMenuActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_CODE = 123;
+
+    @Override
+    protected void onCreate(@Nullable final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.evaluated_menu_activity);
+    }
+
+    public void scan(final View view) {
+        final long permission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if (permission == PackageManager.PERMISSION_DENIED) {
+            final String[] permissions = { Manifest.permission.CAMERA };
+            ActivityCompat.requestPermissions(EvaluatedMenuActivity.this, permissions, PERMISSION_REQUEST_CODE);
+
+        } else {
+            final Intent intent = DecoderActivity.getIntent(this);
+            startActivity(intent);
+        }
+    }
+
+    public void startModal(final View view) {
+        final EvaluatedWelcomeDialog welcomeDialog = new EvaluatedWelcomeDialog(this);
+        welcomeDialog.show();
+    }
+
+    public void showCongrats(final View view) {
+        final Intent congrats = EvaluatedCongratsActivity.getIntent(this);
+        startActivity(congrats);
+    }
+
+    public void startWebView(final View view) {
+        final Intent webView = ExamWebViewActivity.getIntent(this, "http://google.com");
+        startActivity(webView);
+    }
+}
--- app/src/main/java/com/uade/tesis/commons/MainActivity.java
+++ app/src/main/java/com/uade/tesis/commons/MainActivity.java
@@ -1,22 +1,12 @@
 package com.uade.tesis.commons;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import com.uade.tesis.R;
-import com.uade.tesis.evaluated.DecoderActivity;
-import com.uade.tesis.evaluated.EvaluatedCongratsActivity;
-import com.uade.tesis.evaluated.ExamWebViewActivity;
-import com.uade.tesis.evaluated.utils.EvaluatedWelcomeDialog;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final int PERMISSION_REQUEST_CODE = 123;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -25,30 +15,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startEvaluated(final View view) {
-        final long permission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
-        if (permission == PackageManager.PERMISSION_DENIED) {
-            final String[] permissions = { Manifest.permission.CAMERA };
-            ActivityCompat.requestPermissions(MainActivity.this, permissions, PERMISSION_REQUEST_CODE);
-
-        } else {
-            final Intent intent = DecoderActivity.getIntent(this);
-            startActivity(intent);
-        }
+        final Intent intent = new Intent(MainActivity.this, EvaluatedMenuActivity.class);
+        startActivity(intent);
     }
 
-    public void startModal(final View view) {
-        final EvaluatedWelcomeDialog welcomeDialog = new EvaluatedWelcomeDialog(this);
-        welcomeDialog.show();
-    }
-
-    public void showCongrats(final View view) {
-        final Intent congrats = EvaluatedCongratsActivity.getIntent(this);
-        startActivity(congrats);
-    }
-
-    public void startWebView(final View view) {
-        final Intent webView = ExamWebViewActivity.getIntent(this, "http://google.com");
-        startActivity(webView);
+    public void startTeacher(final View view) {
     }
 }
 
