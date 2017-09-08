4b29a472c4b80b9bdb74ab2592ed4cf1e98af0fe
ysansone <yamila.sansone@mercadolibre.com>
2017-09-08 19:16:36 -0300

    Chequeo usuario y contraseña

--- app/src/main/java/com/uade/tesis/evaluator/EvaluatorLogInActivity.java
+++ app/src/main/java/com/uade/tesis/evaluator/EvaluatorLogInActivity.java
@@ -5,13 +5,18 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 import com.uade.tesis.R;
 import com.uade.tesis.evaluator.assignments.AssignmentsActivity;
 
 public class EvaluatorLogInActivity extends AppCompatActivity {
+
+    private EditText user;
+    private EditText password;
 
     @Override
     protected void onCreate(@Nullable final Bundle savedInstanceState) {
@@ -26,8 +31,8 @@ public class EvaluatorLogInActivity extends AppCompatActivity {
     }
 
     private void setUpView() {
-        final EditText user = (EditText) findViewById(R.id.log_in_user);
-        final EditText password = (EditText) findViewById(R.id.log_in_password);
+        user = (EditText) findViewById(R.id.log_in_user);
+        password = (EditText) findViewById(R.id.log_in_password);
         final Button button = (Button) findViewById(R.id.log_in_button);
 
         user.setHint("Usuario");
@@ -36,9 +41,28 @@ public class EvaluatorLogInActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
-                final Intent intent = new Intent(EvaluatorLogInActivity.this, AssignmentsActivity.class);
-                startActivity(intent);
+                if(canLogIn()) {
+                    final Intent intent = new Intent(EvaluatorLogInActivity.this, AssignmentsActivity.class);
+                    startActivity(intent);
+                }
             }
         });
+    }
+
+    private boolean canLogIn() {
+        final boolean isUserEmpty = user.getText() == null || TextUtils.isEmpty(user.getText().toString());
+        final boolean isPasswordEmpty = password.getText() == null || TextUtils.isEmpty(password.getText().toString());
+
+        if (isUserEmpty && isPasswordEmpty) {
+            Toast.makeText(this, "Debe ingresar datos", Toast.LENGTH_LONG).show();
+            return false;
+        } else if (isUserEmpty) {
+                Toast.makeText(this, "Debe ingresar un usuario", Toast.LENGTH_LONG).show();
+            return false;
+        } else if (isPasswordEmpty) {
+            Toast.makeText(this, "Debe ingresar una contraseña", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
     }
 }
